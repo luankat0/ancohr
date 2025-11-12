57104b7a89d9e066dd649b5adb4a2fe1bf8ba519
n3waz <lhk@cesar.school>
2025-11-12 10:37:46 -0300

    feat: added Prisma Database with PostgreSQL

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,9 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Global() // Torna o PrismaService visível globalmente
+@Module({
+    providers: [PrismaService],
+    exports: [PrismaService],
+})
+export class PrismaModule {}
